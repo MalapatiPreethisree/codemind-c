f8072981cacc5201155894ce55b7fa56effedbe7
MalapatiPreethisree <119839549+MalapatiPreethisree@users.noreply.github.com>
2022-12-12 09:52:06 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     float c,t;     if(n<200) {     c=1.20; } else if(n>=200&&n<=400) {     c=1.50; } else if(n>=400&&n<=600) {     c=1.80; } else {     c=2.00; } float b,sc,amt; b=n*c; if(b>400) {     sc=b*0.15; } else {     sc=100; } amt=sc+b; printf("%0.2f",amt); }

---
+++ Current_bill.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    float c,t;
+    if(n<200)
+{
+    c=1.20;
+}    
+else if(n>=200&&n<=400)
+{
+    c=1.50;
+}
+else if(n>=400&&n<=600)
+{
+    c=1.80;
+}
+else
+{
+    c=2.00;
+}
+float b,sc,amt;
+b=n*c;
+if(b>400)
+{
+    sc=b*0.15;
+}
+else
+{
+    sc=100;
+}
+amt=sc+b;
+printf("%0.2f",amt);
+}
